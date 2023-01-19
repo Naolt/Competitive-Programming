c073398f5f1442438f74f29064efb5a72e8a1eed
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-01-19 06:29:38 +0300

    Time: 162 ms (82.21%), Space: 14.6 MB (58.91%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        matchDict = [chr(96)]*len(strs[0])
+        deleteCount = 0
+        
+        for word in strs:
+            for index,letter in enumerate(word):
+                if letter >= matchDict[index]:
+                    matchDict[index] = letter
+                else:
+                    if matchDict[index] != chr(123):
+                        deleteCount += 1
+                    matchDict[index] = chr(123)
+        
+        return deleteCount
+        
