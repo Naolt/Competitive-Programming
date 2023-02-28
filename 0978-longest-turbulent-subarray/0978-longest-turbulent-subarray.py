5c339ae3a0a0c15a1e9ffbb61b8b6ac1a8ccefb6
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-02-28 13:20:33 +0300

    Time: 807 ms (12.30%), Space: 19.2 MB (5.30%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -0,0 +1,83 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        size = len(arr)        
+        result = 1
+        left = 0
+        flag = True
+        
+        if size == 1:
+            return 1
+        if arr[0] > arr[1]:
+            flag = True
+        else:
+            flag = False
+        
+        for right in range(size-1):
+            print(result,left,right)
+            if flag:
+                if right % 2 == 0:
+                    if arr[right] < arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right
+                        flag = False
+                    elif arr[right] == arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right + 1
+                        if left < size-1:
+                            if arr[left] > arr[left+1]:
+                                flag = True
+                            else:
+                                flag = False 
+                    result = max(result,right-left+1)
+                else:
+                    
+                    if arr[right] > arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right
+                        flag = False
+                    elif arr[right] == arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right + 1
+                        if left < size-1:
+                            if arr[left] > arr[left+1]:
+                                flag = True
+                            else:
+                                flag = False 
+                    result = max(result,right-left+1)
+            else:
+                if right % 2 == 0:
+                    if arr[right] > arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right
+                        flag = True
+                    elif arr[right] == arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right + 1
+                        if left < size-1:
+                            if arr[left] > arr[left+1]:
+                                flag = True
+                            else:
+                                flag = False 
+                    result = max(result,right-left+1)
+                else:
+                    
+                    if arr[right] < arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right
+                        flag = True
+                    elif arr[right] == arr[right+1]:
+                        result = max(result,right-left+1)
+                        left = right + 1
+                        if left < size-1:
+                            if arr[left] > arr[left+1]:
+                                flag = True
+                            else:
+                                flag = False 
+                    result = max(result,right-left+1)
+        if left < size-1:
+            result = max(result,size-left)
+                
+        return result
+                    
+                        
+                        
