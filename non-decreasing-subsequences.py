ffac6e38c56e417328216de69d5ea4c5942b4ad6
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-03-23 09:59:37 +0300

    added non-decreasing-subsequences.python

--- non-decreasing-subsequences.py
+++ non-decreasing-subsequences.py
@@ -5,12 +5,10 @@ class Solution:
         return result 
     def backtrack(self,nums,bucket,idx,result):
         size = len(nums)
-        if idx >= size or len(bucket) >= 2:
-            if len(bucket) >= 2:
-                result[tuple(bucket[:])] += 1
-            if idx >= size:
-                print(idx,bucket)
-                return         
+        if len(bucket) >= 2:
+            result[tuple(bucket[:])] += 1
+        if idx >= size:
+            return         
         for i in range(idx,size):
             if not bucket or bucket[-1] <= nums[i]:
                 bucket.append(nums[i])
