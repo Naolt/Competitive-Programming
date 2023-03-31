4d216e1d94ce410a8a94d1c131e231954d271ab8
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-03-31 09:59:22 +0300

    added permutations.python

--- permutations.py
+++ permutations.py
@@ -1,9 +1,9 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
-        self.backtrack(0,nums,[],set(),result)
+        self.backtrack(nums,[],0,result)
         return result
-    def backtrack(self,idx,nums,current,visited,result):
+    def backtrack(self,nums,current,visited,result):
         size1 = len(current)
         size2 = len(nums)
         if size1 == size2:
@@ -11,10 +11,16 @@ class Solution:
             return
         
         for i in range(size2):
-            if nums[i] not in visited:
+            # print(bin(visited),bool(visited & (1<<(nums[i]+10))),nums[i])
+            if not (visited & (1<<(nums[i]+10))):
                 current.append(nums[i])
-                visited.add(nums[i])
-                self.backtrack(i+1,nums,current,visited,result)
+                visited |= (1<<(nums[i]+10))
+                self.backtrack(nums,current,visited,result)
+                # print(current)
                 current.pop()
-                visited.remove(nums[i])
+                # print("removing",nums[i],bin(visited))
+                # print(visited,(1<<(nums[i]+10)))
+                visited -= (1<<(nums[i]+10))
+                # print("after removing",nums[i],bin(visited),visited)
+                # print(current)
         return
