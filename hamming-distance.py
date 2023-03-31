5457319bb0b5b4073944ccd480922e6add2c5cc7
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-03-31 08:49:00 +0300

    added hamming-distance.python

--- hamming-distance.py
+++ hamming-distance.py
@@ -1,7 +1,12 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         count = 0
-        for i in range(32):
-            if (x & (2**i)) != (y & (2**i)):
+        # for i in range(32):
+        #     if (x & (2**i)) != (y & (2**i)):
+        #         count += 1
+        while x or y:
+            if (x^y)&1:
                 count += 1
+            x>>=1
+            y>>=1
         return count
