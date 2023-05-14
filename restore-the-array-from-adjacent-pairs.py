77c043e2755cd75065ce5c8a4fcab45ae5b8ab0c
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-05-14 16:33:38 +0300

    added restore-the-array-from-adjacent-pairs.python

--- restore-the-array-from-adjacent-pairs.py
+++ restore-the-array-from-adjacent-pairs.py
@@ -1,42 +1,23 @@
 class Solution:
     def restoreArray(self, adjacentPairs: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
-        incoming = defaultdict(int)
+        
         for start,end in adjacentPairs:
             graph[start].append(end)
             graph[end].append(start)
-            incoming[start] += 1
-            incoming[end] += 1
-
-        queue = deque()
+            
         topSortedList = []
-
-        for key,value in incoming.items():
-            if value == 1:
-                queue.append(key)
+        for key,value in graph.items():
+            if len(value) == 1:
+                self.dfs(graph,key,topSortedList,set([key]))
+                break
         
-        while queue:
-            leaf = queue.popleft()
-            topSortedList.append(leaf)
-            
-            for nbr in graph[leaf]:
-                incoming[nbr] -= 1
+        return topSortedList
 
-                if incoming[nbr] == 1:
-                    queue.append(nbr)
-        
-        result = [0]*len(incoming)
-        i = 0
-        j = len(result)-1
-        turn = True
+    def dfs(self,graph,current,array,visited):
 
-        for num in topSortedList:
-            if turn:
-                result[i] = num
-                i += 1
-            else:
-                result[j] = num
-                j -= 1
-            
-            turn = not turn
-        return result
+        for nbr in graph[current]:
+            if nbr not in visited:
+                visited.add(nbr)
+                self.dfs(graph,nbr,array,visited)
+        array.append(current)
