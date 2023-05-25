1d057b12007ed99e0c61d7c3cc9ab9536180956a
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-05-25 19:01:25 +0300

    added maximum-element-after-decreasing-and-rearranging.python

--- maximum-element-after-decreasing-and-rearranging.py
+++ maximum-element-after-decreasing-and-rearranging.py
@@ -2,18 +2,12 @@ class Solution:
     def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
 
         arr.sort()
-        
         size = len(arr)
         if arr[0] != 1:
             arr[0] = 1
-        j = 0
-        
-        while j < size-1:
-            i = j
-            while i < size-1 and abs(arr[i+1] - arr[i]) > 1:
-                arr[i+1] = arr[i]+1
-                i += 1
-            j = i
-            j += 1
+            
+        for i in range(size-1):
+            if abs(arr[i]-arr[i+1]) > 1:
+                arr[i+1] = arr[i] + 1
         
         return arr[-1]
