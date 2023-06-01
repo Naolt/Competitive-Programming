fd4ff6ab8257f411d6267eb20ec71c7548ac1282
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-06-01 18:09:38 +0300

    added get-maximum-in-generated-array.python

--- get-maximum-in-generated-array.py
+++ get-maximum-in-generated-array.py
@@ -3,6 +3,8 @@ class Solution:
         self.memo = {}
         self.max = 0
     def getMaximumGenerated(self, n: int) -> int:
+        return self.bu(n)
+        
         for i in range(n):
             self.dp(i+1)
         return self.max
@@ -22,3 +24,19 @@ class Solution:
             self.max = max(self.max,current)
 
         return self.memo[n]
+
+    def bu(self,n):
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        nums = [0]*(n+1)
+        nums[1] = 1
+
+        for i in range(1,n+1):
+            if i*2 <= n:
+                nums[i*2] += nums[i]
+            if i*2+1 <= n:
+                nums[i*2+1] = nums[i]+nums[i+1]
+        
+        return max(nums)
