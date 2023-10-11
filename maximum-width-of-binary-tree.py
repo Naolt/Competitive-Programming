3d75c7f11a743428e1ce72493c468c8a38640516
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-10-11 16:38:39 +0300

    added maximum-width-of-binary-tree.python

--- maximum-width-of-binary-tree.py
+++ maximum-width-of-binary-tree.py
@@ -5,45 +5,17 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.left = -1
-        self.right = -1
-        self.i = 0
-
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        maxDepth = max(self.getDepth(root.left),self.getDepth(root.right))+1
-        def getWidth(root,depth,maxDepth):
-            if depth == 1:
-                if root:
-                    if self.left < 0:
-                        self.left = self.i
-                    else:
-                        self.right = self.i
-                    self.i += 1
-                else:
-                    self.i += 1
-            else:
-                if not root:
-                    self.i += 2**(maxDepth-(maxDepth-depth+1))
-                else:
-                    getWidth(root.left,depth-1,maxDepth)
-                    getWidth(root.right,depth-1,maxDepth)
+        
+        queue = deque([(root,0)])
+        size = 1
+        while queue:
+            node,level = queue.popleft()
+            if node.left:
+                queue.append((node.left,level*2+1))
+            if node.right:
+                queue.append((node.right,level*2+2))
+            if queue:
+                size = max(queue[-1][1]-queue[0][1],size)
 
-        currentMax = 0
-        for i in range(1,maxDepth+1):
-            result = getWidth(root,i,i)
-            if (self.left < 0 or self.right < 0 ) and not (self.left < 0 and self.right < 0):
-                currentMax = max(currentMax,1)
-            elif self.left >= 0 and self.right >=0:
-                currentMax = max(currentMax,self.right-self.left+1)
-            self.left = -1
-            self.right = -1
-            self.i = 0
-        return currentMax 
-    def getDepth(self,root):
-        if not root:
-            return 0
-        left = self.getDepth(root.left)
-        right = self.getDepth(root.right)
-
-        return max(left,right)+1
+        return size+1
