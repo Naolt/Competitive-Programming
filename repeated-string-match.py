b0d1a680e56325f32abf8b60c5db84dd359e7640
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-10-12 21:44:18 +0300

    added repeated-string-match.python

--- repeated-string-match.py
+++ repeated-string-match.py
@@ -1,17 +1,46 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
-        sa = len(a)
-        sb = len(b)
-        div = 1
-        if sa == 1:
-            div = sb*sa
-        else:
-            div = (sb//sa) + 1
-        search = a*div
+        def kmp(pattern, text):
+            n = len(text)
+            m = len(pattern)
+            pi = compute_prefix(pattern)
+            j = 0
+            i = 0
+            ans = 0
+            
+            while i < n:
+                
+                while j > 0 and pattern[j] != text[i]:
+                    if ans > 1:
+                        return -1
+                    j = pi[j-1]
+                if pattern[j] == text[i]:
+                    j += 1
+                
+                if j == m:
+                    return ans + 1
 
-        index = search.find(b)
-
-        if index > -1:
-            return div
-        else:
+                if i == n-1:
+                    if j == 0:
+                        if ans > 1:
+                            return -1
+                    ans += 1
+                    i = -1
+          
+                i += 1
             return -1
+
+        def compute_prefix(pattern):
+            m = len(pattern)
+            pi = [0]*m
+            j = 0
+            for i in range(1, m):
+                while j > 0 and pattern[j] != pattern[i]:
+                    j = pi[j-1]
+                if pattern[j] == pattern[i]:
+                    j += 1
+                pi[i] = j
+            return pi
+        answer = kmp(b,a)
+        
+        return answer
