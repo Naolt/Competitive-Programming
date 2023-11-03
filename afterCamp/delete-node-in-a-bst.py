94eecbdd9750074b225b029276ba47890752ce7b
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2023-11-03 15:55:17 +0300

    Added solution for Delete Node in a BST

---
+++ afterCamp/delete-node-in-a-bst.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return root
+        
+        if root.val > key:
+            root.left = self.deleteNode(root.left,key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right,key)
+        else:
+            if not root.right:
+                return root.left
+            elif not root.left:
+                return root.right
+            
+            nextMin = self.getMinValue(root.right)
+            root.val = nextMin.val
+            root.right = self.deleteNode(root.right,root.val)
+        
+        return root
+
+
+    def getMinValue(self,root):
+        if not root.left:
+            return root
+        return self.getMinValue(root.left)
+
+        
