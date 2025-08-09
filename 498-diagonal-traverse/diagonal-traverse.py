b8ca3a8db8111d9fb316e0c378dadc748999fb07
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2025-08-09 21:06:52 +0300

    Time: 31 ms (12.92%) | Memory: 19.7 MB (61.73%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.py
+++ 498-diagonal-traverse/diagonal-traverse.py
@@ -10,40 +10,26 @@ class Solution:
             return 0 <= i < len(mat) and 0 <= j < len(mat[0])
 
         def getNext(i,j,up):
-            
-            # if I is out of bound
             if up:
+                i += 1
                 if not inbound(0,j):
-                    i += 2
-                    j -= 1
-                else:
                     i += 1
-                
-                return i,j,False
+                    j -= 1
             else:
+                j += 1 
                 if not inbound(i,0):
                     i -= 1
-                    j += 2
-                else:
                     j += 1
                 
-                return i,j,True
-
-                    
+            return i,j, not up
 
         while True:
             res.append(mat[i][j])
-            if up:
-                i -= 1
-                j += 1
-            else:
-                i += 1
-                j -= 1
+            direc = [-1,1] if up else [1,-1]
+            i += direc[0]
+            j += direc[1]
             
             if not inbound(i,j):
                 i,j,up = getNext(i,j,up)
-                # print("new start",i,j,up)
                 if not inbound(i,j):
                     return res
-            # else:
-                # res.append(mat[i][j])
