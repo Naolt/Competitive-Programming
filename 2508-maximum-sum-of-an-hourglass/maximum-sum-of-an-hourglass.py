22443bedf2c8c729017cb67ff9c829c6ea16f89d
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2025-08-15 21:05:55 +0300

    Time: 106 ms (8.91%) | Memory: 20.2 MB (51.16%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -7,27 +7,26 @@ class Solution:
 
         for i in range(row_size-2):
             for j in range(col_size-2):
-                result = self.calcHourTime(grid,i,j)
-                # print((i,j),result)
-                answer = max(answer,result)
+                total = 0
+                for r in range(i,i+3):
+                    for c in range(j,j+3):
+                        # print(f"Element {(i,j)}",grid[i][j])
+                        total += grid[r][c]
+
+                # print(f"removing {(row+1,0)} and {(row+1,col+2)}")
+                total -= grid[i+1][j]
+                total -= grid[i+1][j+2]
+                answer = max(answer,total)
 
         return answer
 
         
 
-    def calcHourTime(self,grid,row,col):
+    # def calcHourTime(self,grid,row,col):
         
-        # print("Start",row,col,grid[row][col])
-        total = 0
-        for i in range(row,row+3):
-            for j in range(col,col+3):
-                # print(f"Element {(i,j)}",grid[i][j])
-                total += grid[i][j]
+    #     # print("Start",row,col,grid[row][col])
+        
 
-        # print(f"removing {(row+1,0)} and {(row+1,col+2)}")
-        total -= grid[row+1][col]
-        total -= grid[row+1][col+2]
-
-        return total
+    #     return total
 
         
