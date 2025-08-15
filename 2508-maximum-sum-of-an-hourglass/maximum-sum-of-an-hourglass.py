d282a8ee3a4e55632a8d2917bbb2c13c944959f1
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2025-08-15 21:00:35 +0300

    Time: 54 ms (20.79%) | Memory: 21 MB (5.28%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -5,26 +5,38 @@ class Solution:
         col_size = len(grid[0])
         answer = 0
 
+        prefix = self.calcPrefix(grid)
+
         for i in range(row_size-2):
             for j in range(col_size-2):
-                result = self.calcHourTime(grid,i,j)
-                print((i,j),result)
-                answer = max(answer,result)
+                bottom_right = prefix[i+3][j+3]
+                bottom_left_prev = prefix[i+3][j]
+                top_right_prev = prefix[i][j+3]
+                top_left_prev = prefix[i][j]
 
+                total = bottom_right - bottom_left_prev - top_right_prev + top_left_prev
+                total -= grid[i+1][j]
+                total -= grid[i+1][j+2]
+
+                answer = max(answer,total)
+                
         return answer
 
         
 
-    def calcHourTime(self,grid,row,col):
+    def calcPrefix(self,grid):
         
-        total = 0
-        for i in range(row,row+3):
-            for j in range(col,col+3):
-                total += grid[i][j]
+        row_size = len(grid)
+        col_size = len(grid[0])
 
-        total -= grid[row+1][col]
-        total -= grid[row+1][col+2]
+        prefix = [[0]*(col_size+1)]
 
-        return total
+        for row in grid:
+            acc = [0]+list(accumulate(row))
+            prefix.append(acc)
 
-        
+        for col in range(len(prefix[0])):
+            for row in range(1,row_size):
+                prefix[row+1][col] += prefix[row][col]
+
+        return prefix
