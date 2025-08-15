ad650ce06792500cc2c6811c1e86f50afab7f4c7
Naol Tamrat <88827066+Naolt@users.noreply.github.com>
2025-08-15 21:04:28 +0300

    Time: 103 ms (10.56%) | Memory: 20.1 MB (51.16%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.py
@@ -5,38 +5,29 @@ class Solution:
         col_size = len(grid[0])
         answer = 0
 
-        prefix = self.calcPrefix(grid)
-
         for i in range(row_size-2):
             for j in range(col_size-2):
-                bottom_right = prefix[i+3][j+3]
-                bottom_left_prev = prefix[i+3][j]
-                top_right_prev = prefix[i][j+3]
-                top_left_prev = prefix[i][j]
+                result = self.calcHourTime(grid,i,j)
+                # print((i,j),result)
+                answer = max(answer,result)
 
-                total = bottom_right - bottom_left_prev - top_right_prev + top_left_prev
-                total -= grid[i+1][j]
-                total -= grid[i+1][j+2]
-
-                answer = max(answer,total)
-                
         return answer
 
         
 
-    def calcPrefix(self,grid):
+    def calcHourTime(self,grid,row,col):
         
-        row_size = len(grid)
-        col_size = len(grid[0])
+        # print("Start",row,col,grid[row][col])
+        total = 0
+        for i in range(row,row+3):
+            for j in range(col,col+3):
+                # print(f"Element {(i,j)}",grid[i][j])
+                total += grid[i][j]
 
-        prefix = [[0]*(col_size+1)]
+        # print(f"removing {(row+1,0)} and {(row+1,col+2)}")
+        total -= grid[row+1][col]
+        total -= grid[row+1][col+2]
 
-        for row in grid:
-            acc = [0]+list(accumulate(row))
-            prefix.append(acc)
+        return total
 
-        for col in range(len(prefix[0])):
-            for row in range(1,row_size):
-                prefix[row+1][col] += prefix[row][col]
-
-        return prefix
+        
